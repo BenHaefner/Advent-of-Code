b93bf54bf5d4c24bbe16ac6af63d1b2d18f4ef69
BenHaefner <ibenroy2@gmail.com>
2019-12-01 19:10:56 -0500

    Inital Commit

---
+++ AdventOfCode2019.py
@@ -0,0 +1,12 @@
+total_fuel = 0
+
+with open('D1P1.txt', 'r') as file: 
+    for line in file:
+        item = int(line)
+        check = 1
+        while check > 0:
+            item = int(item/3) - 2
+            total_fuel = total_fuel + item
+            check = int(item/3) - 2
+
+print(total_fuel)
