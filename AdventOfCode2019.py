8bbad12fcf8c4ca07ea9d65ac2385ca05b252aed
BenHaefner <ibenroy2@gmail.com>
2019-12-01 19:29:46 -0500

    Making things slightly more modular

--- AdventOfCode2019.py
+++ AdventOfCode2019.py
@@ -1,12 +1,15 @@
 total_fuel = 0
 
+def fuelCalc(item):
+    return int(item/3) - 2
+
 with open('input.txt', 'r') as file: 
     for line in file:
         item = int(line)
-        check = 1
+        check = fuelCalc(item)
         while check > 0:
-            item = int(item/3) - 2
+            item = check
+            check = fuelCalc(item)
             total_fuel = total_fuel + item
-            check = int(item/3) - 2
 
 print(total_fuel)
