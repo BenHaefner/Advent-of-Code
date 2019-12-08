4c5d7669ba51690e4cd93f77b0d8aba1ac453f4c
BenHaefner <ibenroy2@gmail.com>
2019-12-07 20:36:03 -0500

    Part 1 done for day 5

--- 2019/Day5/Part1.py
+++ 2019/Day5/Part1.py
@@ -1,42 +1,70 @@
 inputArray = []
+index = 0
+def addCase(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = inputArray[index1] + inputArray[index2]
+    else:
+        inputArray[outputIndex] = (inputArray[index1[0]] if index1[1] == 0 else index1[0]) + (inputArray[index2[0]] if index2[1] == 0 else index2[0])
 
-def addCase(index1, index2, outputIndex):
-    inputArray[outputIndex] = inputArray[index1] + inputArray[index2]
+def multCase(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = inputArray[index1] * inputArray[index2]
+    else:
+        inputArray[outputIndex] = (inputArray[index1[0]] if index1[1] == 0 else index1[0]) * (inputArray[index2[0]] if index2[1] == 0 else index2[0])
 
-def multCase(index1, index2, outputIndex):
-    inputArray[outputIndex] = inputArray[index1] * inputArray[index2]
+def singleIntInput(address):
+    inputArray[address] = int(input("Whats the input value: ")) 
 
-def singleIntInput(address, value):
-    inputArray[address] = value 
+def singleIntOutput(addressValue, senderType):
+    if senderType == 0:
+        print(inputArray[addressValue])
+    else:
+        print(addressValue)
 
-def singleIntOutput(address):
-    return inputArray[address]
+def opCodeOnly():
+    global index
+    if inputArray[index] is 1:
+        addCase(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+        index = index + 4
+    elif inputArray[index] is 2:
+        multCase(inputArray[index+1],inputArray[index+2],inputArray[index+3], 0)
+        index = index + 4
+    elif inputArray[index] is 3:
+        singleIntInput(inputArray[index + 1])
+        index = index + 2
+    elif inputArray[index] is 4:
+        singleIntOutput(inputArray[index+1], 0)
+        index = index + 2
+    else:
+        pass
+
+def opCodeAndParams():
+    global index
+    inputParams, opCode = str(inputArray[index])[:-2], int(str(inputArray[index])[-2:])
+    if 1 == opCode:
+        addCase((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    elif 2 == opCode:
+        print(inputParams)
+        multCase((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    elif 3 == opCode:
+        index = index + 2
+    elif 4 == opCode:
+        singleIntOutput(inputArray[index+1], 1)
+        index = index + 2
+    else:
+        pass
 
 with open('input.txt', 'r') as file: 
     for line in file:
         inputArray = [int(x) for x in line.split(',')]
 
-inputArray[1] = int(input('What was in position 1? Integers only please! '))
-inputArray[2] = int(input('What was in position 2? Integers only please! '))
-
-index = 0
 while True:
-    inputParams, opCode = str(inputArray[index])[:-2], int(str(inputArray[index])[-2:])
-    print(opCode)
-    print(inputParams)
-    if inputArray[index] is 1:
-        addCase(inputArray[index+1],inputArray[index+2],inputArray[index+3])
-    elif inputArray[index] is 2:
-        multCase(inputArray[index+1],inputArray[index+2],inputArray[index+3])
-    elif inputArray[index] is 3:
-        pass
-    elif inputArray[index] is 4:
-        pass
-    elif inputArray[index] is 99:
+    if inputArray[index] is 99:
         break
+    if len(str(inputArray[index])) > 2:
+        opCodeAndParams()
     else:
-        print('Something went wrong')
-        break
-    index = index + 4
-
-print(inputArray[0])
+        opCodeOnly()
+print('Program Ended')
--- 2019/Day5/Part2.py
+++ 2019/Day5/Part2.py
@@ -1,32 +1,117 @@
 inputArray = []
-testingArray = []
+index = 0
+def addCase(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = inputArray[index1] + inputArray[index2]
+    else:
+        inputArray[outputIndex] = (inputArray[index1[0]] if index1[1] == 0 else index1[0]) + (inputArray[index2[0]] if index2[1] == 0 else index2[0])
 
-def addCase(index1, index2, outputIndex):
-    testingArray[outputIndex] = testingArray[index1] + testingArray[index2]
+def multCase(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = inputArray[index1] * inputArray[index2]
+    else:
+        inputArray[outputIndex] = (inputArray[index1[0]] if index1[1] == 0 else index1[0]) * (inputArray[index2[0]] if index2[1] == 0 else index2[0])
 
-def multCase(index1, index2, outputIndex):
-    testingArray[outputIndex] = testingArray[index1] * testingArray[index2]
+def singleIntInput(address):
+    inputArray[address] = int(input("Whats the input value: ")) 
+
+def singleIntOutput(addressValue, senderType):
+    if senderType == 0:
+        print(inputArray[addressValue])
+    else:
+        print(addressValue)
+
+def jumpTrue(index1, index2, outputIndex, senderType,):
+    if senderType == 0:
+        index = inputArray[index2] if inputArray[index1] != 0 else inputArray[outputIndex]
+        if index != inputArray[index2]:
+            index += 3
+    else: 
+        pass
+
+def jumpFalse(index1, index2, outputIndex, senderType,):
+    if senderType == 0:
+        index = inputArray[index2] if inputArray[index1] == 0 else inputArray[outputIndex]
+        if index != inputArray[index2]:
+            index += 3
+    else: 
+        pass
+
+def lessThan(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = 1 if inputArray[index1] < inputArray[index2] else 0
+    else: 
+        pass
+
+def equals(index1, index2, outputIndex, senderType):
+    if senderType == 0:
+        inputArray[outputIndex] = 1 if inputArray[index1] == inputArray[index2] else 0
+    else: 
+        pass
+
+def opCodeOnly():
+    global index
+    if inputArray[index] is 1:
+        addCase(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+        index = index + 4
+    elif inputArray[index] is 2:
+        multCase(inputArray[index+1],inputArray[index+2],inputArray[index+3], 0)
+        index = index + 4
+    elif inputArray[index] is 3:
+        singleIntInput(inputArray[index + 1])
+        index = index + 2
+    elif inputArray[index] is 4:
+        singleIntOutput(inputArray[index+1], 0)
+        index = index + 2
+    elif inputArray[index] is 5:
+        jumpTrue(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+    elif inputArray[index] is 6:
+        jumpFalse(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+    elif inputArray[index] is 7:
+        lessThan(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+        index = index + 4
+    elif inputArray[index] is 8:
+        equals(inputArray[index+1],inputArray[index+2],inputArray[index+3],0)
+        index = index + 4
+    else:
+        pass
+
+def opCodeAndParams():
+    global index
+    inputParams, opCode = str(inputArray[index])[:-2], int(str(inputArray[index])[-2:])
+    if 1 == opCode:
+        addCase((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    elif 2 == opCode:
+        multCase((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    elif 3 == opCode:
+        index = index + 2
+    elif 4 == opCode:
+        singleIntOutput(inputArray[index+1], 1)
+        index = index + 2
+    elif 5 == opCode:
+        jumpTrue((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+    elif 6 == opCode:
+        jumpFalse((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+    elif 7 == opCode:
+        lessThan((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    elif 8 == opCode:
+        equals((inputArray[index+1], int(inputParams[-1]) if inputParams[-1] else 0 ),(inputArray[index+2], int(inputParams[-2]) if len(inputParams) > 1 else 0 ),inputArray[index+3], 1)
+        index = index + 4
+    else:
+        pass
 
 with open('input.txt', 'r') as file: 
     for line in file:
         inputArray = [int(x) for x in line.split(',')]
 
-for noun in range(100):
-    for verb in range(100):
-        testingArray = inputArray.copy()
-        testingArray[1] = noun
-        testingArray[2] = verb
-        index = 0
-        while True:
-            if testingArray[index] == 1:
-                addCase(testingArray[index+1],testingArray[index+2],testingArray[index+3])
-            elif testingArray[index] == 2:
-                multCase(testingArray[index+1],testingArray[index+2],testingArray[index+3])
-            elif testingArray[index] == 99:
-                break
-            else:
-                break
-            index = index + 4
-        
-        if testingArray[0] == 19690720:
-            print(100*noun+verb)
+while True:
+    if inputArray[index] is 99:
+        break
+    if len(str(inputArray[index])) > 2:
+        opCodeAndParams()
+    else:
+        opCodeOnly()
+print('Program Ended')
